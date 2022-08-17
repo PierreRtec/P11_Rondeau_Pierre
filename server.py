58a955c9931fa6ef8d3e5aa9a0840d11451ff901
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-08-17 10:57:08 +0200

    [FIX] #05 Reprise branche 04

--- server.py
+++ server.py
@@ -1,59 +1,92 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from datetime import datetime
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 
 def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+    with open("clubs.json") as c:
+        listOfClubs = json.load(c)["clubs"]
+        return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+    with open("competitions.json") as comps:
+        listOfCompetitions = json.load(comps)["competitions"]
+        return listOfCompetitions
 
 
 app = Flask(__name__)
-app.secret_key = 'something_special'
+app.secret_key = "something_special"
 
 competitions = loadCompetitions()
 clubs = loadClubs()
 
-@app.route('/')
+actual_date = str(datetime.now())
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
-
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    return render_template("index.html")
 
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+@app.route("/showSummary", methods=["POST"])
+def show_summary() -> str:
+    try:
+        club = [club for club in clubs if club["email"] == request.form["email"]][0]
+        return render_template(
+            "welcome.html", club=club, competitions=competitions, date=actual_date
+        )
+    except IndexError:
+        return render_template(
+            "index.html", error_message="Sorry, that email wasn't found. Try again."
+        )
+
+
+@app.route("/book/<competition>/<club>")
+def book(competition, club):
+    foundClub = [c for c in clubs if c["name"] == club][0]
+    foundCompetition = [c for c in competitions if c["name"] == competition][0]
+    if foundClub and foundCompetition and foundCompetition["date"] >= actual_date:
+        return render_template(
+            "booking.html", club=foundClub, competition=foundCompetition
+        )
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template(
+            "welcome.html", club=club, competitions=competitions, date=actual_date
+        )
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    competition = [c for c in competitions if c["name"] == request.form["competition"]][
+        0
+    ]
+    club = [c for c in clubs if c["name"] == request.form["club"]][0]
+    placesRequired = int(request.form["places"])
+    placesCompetition = int(competition["numberOfPlaces"])
+    clubPoints = int(club["points"])
+    if placesRequired <= 0:
+        flash("Ne peut être inférieur ou égal à 0")
+    elif placesRequired > 12:
+        flash("Vous ne pouvez pas réserver plus de 12 places à la fois.")
+    elif placesRequired >= placesCompetition:
+        flash(
+            "Attention, vous avez selectionner plus de places que le nombre de place maximum."
+        )
+    elif clubPoints < placesRequired:
+        flash("Vous n'avez pas assez de points")
+    else:
+        competition["numberOfPlaces"] = placesCompetition - placesRequired
+        flash("Great-booking complete!")
+    return render_template(
+        "welcome.html", club=club, competitions=competitions, date=actual_date
+    )
 
 
 # TODO: Add route for points display
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
---
+++ tests/test_server.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+
+from assertpy import assert_that
+
+import server
+
+
+class TestShowSummary:
+    def test_show_summary_unknown_email_address(
+        self,
+        client,
+    ):
+        # expectations
+        expected_status_code = 200
+
+        # method_call
+        result = client.post("/showSummary", data={"email": "unknown@random.com"})
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
+    def test_show_summary_unknown_email_address_not_found(
+        self,
+        client,
+    ):
+        # initialisation
+        email_not_found = "Sorry, that email wasn&#39;t found. Try again."
+
+        # method_call
+        result = client.post("/showSummary", data={"email": "unkezezanown@zae.com"})
+
+        # assertions
+        assert_that(result.data.decode()).contains(email_not_found)
+
+
+class TestPurchasePlaces:
+    def test_purchase_places_nominal(self, client):
+        # expectations
+        expected_status_code = 200
+
+        # method_call
+        result = client.post(
+            "/purchasePlaces",
+            data={
+                "competition": "Spring Festival",
+                "club": "Iron Temple",
+                "places": 12,
+            },
+        )
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
+    def test_purchase_places_over_12(self, client):
+        # initialisation
+        competition = server.competitions[1]
+        club = server.clubs[2]
+        # expectations
+        expected_status_code = 200
+
+        # method_call
+        result = client.post(
+            "/purchasePlaces",
+            data={
+                "competition": competition["name"],
+                "club": club["name"],
+                "places": 15,
+            },
+        )
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
+
+class TestBook:
+    def test_book_nominal_date(self, client):
+        # expected
+        expected_status_code = 200
+
+        # method_call
+        result = client.get("/book/Fall Classic/Simply Lift")
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
+    def test_book_date_past(self, client):
+        # expected
+        expected_status_code = 200
+        error_message = "Something went wrong-please try again"
+        # method_call
+        result = client.get("/book/Spring Festival/Simply Lift")
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+        assert_that(result.data.decode()).contains(error_message)
