fef0ebcab9691c15e392df5ca7c8aa2b7083cc7c
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-08-18 10:03:33 +0200

    [DEV] Corrections et coverage à 100%

--- tests/tests_integration/test_server.py
+++ tests/tests_integration/test_server.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from assertpy import assert_that
 
 import server
@@ -32,29 +30,71 @@ class TestShowSummary:
         # assertions
         assert_that(result.data.decode()).contains(email_not_found)
 
+    def test_show_summary_ok(
+        self,
+        client,
+    ):
+        # initialisation
+        email = "admin@irontemple.com"
+
+        # expectation
+        expected_status_code = 200
+
+        # method_call
+        result = client.post("/showSummary", data={"email": email})
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
 
 class TestPurchasePlaces:
-    def test_purchase_places_nominal(self, client):
+    def test_purchase_places_zero(self, client):
         # expectations
         expected_status_code = 200
+        club = server.clubs[1]
+        comp = server.competitions[1]
 
         # method_call
         result = client.post(
             "/purchasePlaces",
             data={
-                "competition": "Spring Festival",
-                "club": "Iron Temple",
-                "places": 12,
+                "competition": comp["name"],
+                "club": club["name"],
+                "places": 0,
             },
         )
+        error_message = "Ne peut être inférieur ou égal à 0"
 
         # assertions
         assert_that(result.status_code).is_equal_to(expected_status_code)
+        assert_that(result.data.decode()).contains(error_message)
 
     def test_purchase_places_over_12(self, client):
+        # expectations
+        expected_status_code = 200
+        club = server.clubs[0]
+        comp = server.competitions[0]
+
+        # method_call
+        result = client.post(
+            "/purchasePlaces",
+            data={
+                "competition": comp["name"],
+                "club": club["name"],
+                "places": 13,
+            },
+        )
+        error_message = "Vous ne pouvez pas réserver plus de 12 places à la fois."
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+        assert_that(result.data.decode()).contains(error_message)
+
+    def test_purchase_places_over_places_comp(self, client):
         # initialisation
         competition = server.competitions[1]
-        club = server.clubs[2]
+        club = server.clubs[0]
+
         # expectations
         expected_status_code = 200
 
@@ -64,12 +104,39 @@ class TestPurchasePlaces:
             data={
                 "competition": competition["name"],
                 "club": club["name"],
-                "places": 15,
+                "places": 27,
             },
         )
 
+        error_message = "Attention, vous avez selectionner plus de places que le nombre de place maximum."
+
         # assertions
         assert_that(result.status_code).is_equal_to(expected_status_code)
+        assert_that(result.data.decode()).contains(error_message)
+
+    def test_purchase_places_not_enought_club_points(self, client):
+        # initialisation
+        competition = server.competitions[1]
+        club = server.clubs[1]
+
+        # expectations
+        expected_status_code = 200
+
+        # method_call
+        result = client.post(
+            "/purchasePlaces",
+            data={
+                "competition": competition["name"],
+                "club": club["name"],
+                "places": 5,
+            },
+        )
+
+        error_message = "Vous n&#39;avez pas assez de points"
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+        assert_that(result.data.decode()).contains(error_message)
 
     def test_purchase_places_pts_club_nominal(self, client):
         # initialisation
@@ -114,3 +181,39 @@ class TestBook:
         # assertions
         assert_that(result.status_code).is_equal_to(expected_status_code)
         assert_that(result.data.decode()).contains(error_message)
+
+
+class TestClubTable:
+    def test_club_table_nominal(self, client):
+        # expected
+        expected_status_code = 200
+
+        # method_call
+        result = client.get("/displayboard")
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
+
+class TestLogout:
+    def test_logout_nominal(self, client):
+        # expected
+        expected_status_code = 302
+
+        # method_call
+        result = client.get("/logout")
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
+
+
+class TestIndex:
+    def test_index_nominal(self, client):
+        # expected
+        expected_status_code = 200
+
+        # method_call
+        result = client.get("/")
+
+        # assertions
+        assert_that(result.status_code).is_equal_to(expected_status_code)
